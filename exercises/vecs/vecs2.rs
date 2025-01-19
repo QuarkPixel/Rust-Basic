a834dd415668faa2d27563bc57ee4a6ddaf356c5
Xuancong Meng <65812149+QuarkPixel@users.noreply.github.com>
2025-01-19 07:31:12 +0000

    update

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -7,13 +7,12 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for element in v.iter_mut() {
+    for (i, element) in v.iter_mut().enumerate() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element = ((i + 1) * 4) as i32
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -21,11 +20,15 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        ???
-    }).collect()
+    let mut i = 0;
+    v.iter()
+        .map(|element| {
+            // TODO: Do the same thing as above - but instead of mutating the
+            // Vec, you can just return the new number!
+            i += 1;
+            i * 4
+        })
+        .collect()
 }
 
 #[cfg(test)]
